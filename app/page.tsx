8dbf4fa45f958307e154883d08abbb23d625858c
Ali Mansoori <71168717+ali-mansori-dev@users.noreply.github.com>
2024-11-27 22:43:56 +0330

    implement ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@mantine/core";
+import { Button, TextInput } from "@mantine/core";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid">
+      <Button color="pink">Click me</Button>
       <Button>Click me</Button>
-      <Button>Click me</Button>
+      <TextInput label="Input label" description="Input description" />
     </div>
   );
 }
